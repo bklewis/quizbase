de4803209417c72ee6d3aa67c10f4d92f4dec1af
Lewis <bklewis@rigel.cs.brynmawr.edu>
2016-02-22 13:16:42 -0500

    Set up models

--- quizbase/quiz/models.py
+++ quizbase/quiz/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+
+class Quiz(models.Model)
+	name = models.CharField(max_length=256)
+
+class Question(models.Model)
+	quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+	string = models.CharField(max_length=1000)
+	ord = models.IntegerField()
+
+class Answer(models.Model)
+	question = models.ForeignKey(Question, on_delete=models.CASCADE)
+	string = models.CharField(max_length=1000)
+	correctness = models.IntegerField()
+
+class User(models.Model)
+	first_name = models.CharField(max_length=50)
+	last_name = models.CharField(max_length=50)
+	email = models.CharField(max_length=500)
+	permissions = models.IntegerField()
+
+class Quiz_attempt(models.Model)
+	quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	attempt_no = models.IntegerField()
+	score = models.IntegerField()
+	start_time = models.DateTimeField(default=datetime.now, blank=True)
+	end_time = models.DateTimeField()
+
+class Answer_attempt(models.Model)
+	answer = models.ForeignKey(Answer, on_delete=models.CASCADE)
+	quiz_attempt = models.ForeignKey(Quiz_attempt, on_delete=models.CASCADE)
+	submit_time = models.DateTimeField(default=datetime.now, blank=True)
