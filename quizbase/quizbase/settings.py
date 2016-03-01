bbec49bacb74ac55864e359f79fc9e2c1940f472
Bryce Lewis <bklewis@brynmawr.edu>
2016-03-01 13:49:41 -0500

    Updated time zone

--- quizbase/quizbase/settings.py
+++ quizbase/quizbase/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/New_York'
 
 USE_I18N = True
 
@@ -99,3 +99,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+#STATIC_ROOT = os.path.join(BASE_DIR,'static')
