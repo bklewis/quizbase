ea9b315b4847f6dd2b666b40d5768bd22be366ac
Bryce Lewis <bklewis@brynmawr.edu>
2016-03-23 18:29:37 -0400

    Added quiz admin - didn't seem to change anything

--- quizbase/quiz/admin.py
+++ quizbase/quiz/admin.py
@@ -7,8 +7,13 @@ from .models import Answer
 from .models import Quiz_attempt
 from .models import Answer_attempt
 
-admin.site.register(Quiz)
+#admin.site.register(Quiz)
 admin.site.register(Question)
 admin.site.register(Answer)
 admin.site.register(Quiz_attempt)
 admin.site.register(Answer_attempt)
+
+class QuizAdmin(admin.modelAdmin):
+	fields: ['name']
+
+admin.site.register(Quiz, QuizAdmin)
