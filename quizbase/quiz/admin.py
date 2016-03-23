d6c306419d7945b7f541c75f3c4107048509ff8e
Bryce Lewis <bklewis@brynmawr.edu>
2016-03-23 13:48:01 -0400

    Adding models to admin

--- quizbase/quiz/admin.py
+++ quizbase/quiz/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Quiz
+from .models import Question
+from .models import Answer
+from .models import Quiz_attempt
+from .models import Answer_attempt
+
+admin.site.register(Quiz)
