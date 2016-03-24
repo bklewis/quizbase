776c2dbb2f6184170ddf95828a7c897c622c7fac
Bryce Lewis <bklewis@brynmawr.edu>
2016-03-23 22:46:29 -0400

    Multiple get pages functioning - working on post now

--- quizbase/quiz/views.py
+++ quizbase/quiz/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Quiz
 
 # Create your views here.
 
@@ -7,4 +10,16 @@ def index(request):
 	return HttpResponse("Hello, world.  You're at the quiz index.")
 
 def create(request):
-	return HttpResponse("Hello, this is create!")
+	if request.method == 'GET':
+		quizList = Quiz.objects.order_by('name')
+		#output = ', '.join([q.name for q in quizList])
+		#return HttpResponse(output)
+		#template = loader.get_template('create.html')
+		context = {'quizList': quizList,}
+		#return HttpResponse(template.render(context, request))
+		return render(request, 'create.html', context)
+	elif request.method == 'POST':
+		return HttpResponse("post!")
+
+def creating(request):
+	return HttpResponse("CREATING")
