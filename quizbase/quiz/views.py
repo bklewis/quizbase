028d0b5b353de759fb834c6af489c7b3b93a1dfc
Bryce Lewis <bklewis@brynmawr.edu>
2016-03-24 00:04:11 -0400

    Nixed the 403 error by adding a csrf_token - things still aren't going to the database

--- quizbase/quiz/views.py
+++ quizbase/quiz/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
+from django.shortcuts import render_to_response
+from django.template.context_processors import csrf
+
 from .models import Quiz
 
 # Create your views here.
@@ -10,16 +13,23 @@ def index(request):
 	return HttpResponse("Hello, world.  You're at the quiz index.")
 
 def create(request):
-	if request.method == 'GET':
-		quizList = Quiz.objects.order_by('name')
-		#output = ', '.join([q.name for q in quizList])
-		#return HttpResponse(output)
-		#template = loader.get_template('create.html')
-		context = {'quizList': quizList,}
-		#return HttpResponse(template.render(context, request))
-		return render(request, 'create.html', context)
-	elif request.method == 'POST':
-		return HttpResponse("post!")
+	c = {}
+	c.update(csrf(request))
+	#quiz = Quiz(name = request.POST['quizname'])
+	#quiz.save()
+	#if request.method == 'GET':
+	quizList = Quiz.objects.order_by('name')
+	#output = ', '.join([q.name for q in quizList])
+	#return HttpResponse(output)
+	#template = loader.get_template('create.html')
+	context = {'quizList': quizList,}
+	#return HttpResponse(template.render(context, request))
+	return render(request, 'create.html', context)
+	#return render_to_response(request, 'create.html', c)
+	# https://docs.djangoproject.com/en/1.8/ref/csrf/#how-to-use-it
+
+	#elif request.method == 'POST':
+	#	return HttpResponse("post!")
 
 def creating(request):
 	return HttpResponse("CREATING")
