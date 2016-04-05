8cad0152940a38728db2db7275da2ac84fdac347
Bryce Lewis <bklewis@brynmawr.edu>
2016-04-05 17:22:41 -0400

    Adding logout

--- quizbase/quiz/urls.py
+++ quizbase/quiz/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
 	url(r'^quizzes/', views.quizzes, name='quizzes'),
 #	url(r'^login/', login),
 	url(r'^login/', login, {'template_name': 'login.html'}),
+	url(r'^logout/', logout),
 #	url(r'^login/', views.login, name='login'),
 #	url(r'^login/', views.LoginView.as_view(), name='login'),
 ]
