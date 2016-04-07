f3d7edb81bbabc7a320d0482fd60a737877670ac
Bryce Lewis <bklewis@brynmawr.edu>
2016-04-07 13:29:25 -0400

    Last commit with non-default login code

--- quizbase/quiz/views.py
+++ quizbase/quiz/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 from django.template import loader
 from django.contrib import auth
 from django.contrib.auth.decorators import login_required
+from django.views.generic.list import ListView
+from django.utils import timezone
 
 from django.shortcuts import render_to_response
 from django.template.context_processors import csrf
