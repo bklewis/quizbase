dab056040a908ad8b0369ec41ce415abe5b9f11c
Bryce Lewis <bklewis@brynmawr.edu>
2016-04-21 09:16:21 -0400

    Wrote method to score quiz attempt (untested)

--- quizbase/quiz/models.py
+++ quizbase/quiz/models.py
@@ -44,6 +44,25 @@ class Quiz_attempt(models.Model):
 	start_time = models.DateTimeField(default=datetime.now, blank=True)
 	end_time = models.DateTimeField()
 
+	def getScore(self):
+		"Returns score for quiz attempt"
+		quiz = self.quiz
+		questionList = Question.objects.filter(quiz=quiz.id)
+		aaList = Answer_attempt.objects.filter(quiz_attempt=self.id)
+		score = 0
+		for q in questionList:
+			vList = []
+			answerList = Answer.objects.filter(question=q.id)
+			for a in answerList:
+				if aa.answer == a.id:
+					vList.append(a.value)
+			twos = vList.count(2)
+			ones = vList.count(1)
+			zeros = vList.count(0)
+			score += twos * 1/(2 ** ones) * (0 ** zeros)
+		return score
+
+
 class Answer_attempt(models.Model):
 	answer = models.ForeignKey(Answer, on_delete=models.CASCADE)
 	quiz_attempt = models.ForeignKey(Quiz_attempt, on_delete=models.CASCADE)
