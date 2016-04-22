5b23eeafbb5806a308b55a54fe345764e7c6e5aa
Bryce Lewis <bklewis@brynmawr.edu>
2016-04-21 20:32:29 -0400

    Pep8ified forms

--- quizbase/quiz/forms.py
+++ quizbase/quiz/forms.py
@@ -3,45 +3,46 @@ from django.forms import formset_factory
 
 from .models import Quiz, Question, Answer, Quiz_attempt, Answer_attempt
 
+
 class QuestionForm(forms.ModelForm):
 
-	class Meta:
-		model = Question
-		fields = ('string', 'quiz')
-		widgets = {'string': forms.Textarea(),
-				'quiz': forms.HiddenInput()}
+    class Meta:
+        model = Question
+        fields = ('string', 'quiz')
+        widgets = {'string': forms.Textarea(),
+                   'quiz': forms.HiddenInput()}
 
-	def __init__(self, *args, **kwargs):
-		super(QuestionForm, self).__init__(*args, **kwargs)
-		self.fields['string'].label = "Add a new question"
+    def __init__(self, *args, **kwargs):
+        super(QuestionForm, self).__init__(*args, **kwargs)
+        self.fields['string'].label = "Add a new question"
+
 
 class AnswerForm(forms.ModelForm):
 
-	class Meta:
-		model = Answer
-		fields = ('string', 'question', 'value')
-		widgets = {'string': forms.Textarea(),
-			'question': forms.HiddenInput(),
-			'value': forms.Select(choices=Answer.VALUE_CHOICES)}
-#			'value': forms.ChoiceField(choices=Answer.VALUE_CHOICES)}
+    class Meta:
+        model = Answer
+        fields = ('string', 'question', 'value')
+        widgets = {'string': forms.Textarea(),
+                   'question': forms.HiddenInput(),
+                   'value': forms.Select(choices=Answer.VALUE_CHOICES)}
 
-	def __init__(self, *args, **kwargs):
-		super(AnswerForm, self).__init__(*args, **kwargs)
-		self.fields['string'].label = "Answer Text"
-		self.fields['value'].label = "Answer Value"
-		self.fields['value'].queryset = Answer.VALUE_CHOICES
+    def __init__(self, *args, **kwargs):
+        super(AnswerForm, self).__init__(*args, **kwargs)
+        self.fields['string'].label = "Answer Text"
+        self.fields['value'].label = "Answer Value"
+        self.fields['value'].queryset = Answer.VALUE_CHOICES
+
 
 class QaForm(forms.Form):
-	#name = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple, choice=
-	def __init__(self, question, *args, **kwargs):
-		self.question = question
-		self.answers = forms.ModelMultipleChoiceField(queryset=Answer.objects.filter(question=question.id))
-		
+    # name = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple, choice=
 
+    def __init__(self, question, *args, **kwargs):
+        self.question = question
+        self.answers = forms.ModelMultipleChoiceField(queryset=Answer.objects.filter(question=question.id))
 
-'''		
+'''
 class QaForm(forms.Form):
-	def __init__(self, data, questions, *args, **kwargs):
-		self.questions = questions
-		for question in questions:
+    def __init__(self, data, questions, *args, **kwargs):
+        self.questions = questions
+        for question in questions:
 '''
