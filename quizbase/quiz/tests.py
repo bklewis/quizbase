630b528570e83f499d1628d2e6edf1897e1c8e4b
Bryce Lewis <bklewis@brynmawr.edu>
2016-04-29 23:57:41 -0400

    More test things

--- quizbase/quiz/tests.py
+++ quizbase/quiz/tests.py
@@ -17,15 +17,8 @@ from .views import *
 class BasicTests(TestCase):
 
     def test_index_page(self):
-        # response = self.client.get('/quizzes/', follow=True)
-        # self.assertRedirects(response, '/login/')
         found = resolve('/')
         self.assertEqual(found.func, index)
-
-    # def test_user(self):
-    #     myUser = User.objects.create(name='admin')
-    #     user = User.objects.get(name='admin')
-    #     self.assertEqual(myUser, user)
 
 
 class Login(TestCase):
@@ -36,8 +29,6 @@ class Login(TestCase):
 
     def test_user_creation(self):
         self.client.login(username=self.admin.username, password=self.admin.password)
-        # self.assertEqual(self.client.user, self.admin)
-        # self.assertIn('_auth_user_id', self.client.session)
         self.assertEqual(self.admin.is_staff, True)
 
     def test_page_protection(self):
@@ -58,6 +49,7 @@ class CreateThings(TestCase):
 
     def test_login_view(self):
         response = self.client.post('/postquiz/', {'name': 'testquiz'})
+        quiz = self.client.get('/')
 
 
 class GetThings(TestCase):
