2cb54f18d48a76879fc36624fb00180fe7ecd38c
Bryce Lewis <bklewis@brynmawr.edu>
2016-04-29 23:49:10 -0400

    Some testing things

--- quizbase/quiz/tests.py
+++ quizbase/quiz/tests.py
@@ -1,3 +1,4 @@
+"""All the Tests."""
 from django.test import TestCase
 from django.test import Client
 
@@ -27,22 +28,48 @@ class BasicTests(TestCase):
     #     self.assertEqual(myUser, user)
 
 
-class CreateQuiz(TestCase):
+class Login(TestCase):
 
     def setUp(self):
         self.admin = User.objects.create_superuser('admin', 'test@email.com', 'password')
         self.admin.save()
-        # Quiz.objects.create(name="quiz")
 
-    def test_index(self):
-        response = self.client.get('/')
-        self.assertEqual(302, response.status_code)
+    def test_user_creation(self):
         self.client.login(username=self.admin.username, password=self.admin.password)
+        # self.assertEqual(self.client.user, self.admin)
+        # self.assertIn('_auth_user_id', self.client.session)
         self.assertEqual(self.admin.is_staff, True)
-        response = self.client.get('')
-        # self.assertEqual(200, response.status_code)
+
+    def test_page_protection(self):
+        response = self.client.get('/')
+        self.assertRedirects(response, '/login/?next=/')
+
+    def test_login_view(self):
+        response = self.client.post('/login/', {'username': 'admin', 'password': 'password'})
+        self.assertRedirects(response, '/')
 
 
-# Inputting a quiz
-# Taking a quiz
-# Score
+class CreateThings(TestCase):
+
+    def setUp(self):
+        self.admin = User.objects.create_superuser('admin', 'test@email.com', 'password')
+        self.admin.save()
+        self.client.login(username=self.admin.username, password=self.admin.password)
+
+    def test_login_view(self):
+        response = self.client.post('/postquiz/', {'name': 'testquiz'})
+
+
+class GetThings(TestCase):
+
+    def setUp(self):
+        self.admin = User.objects.create_superuser('admin', 'test@email.com', 'password')
+        self.admin.save()
+        self.client.login(username=self.admin.username, password=self.admin.password)
+        quiz = Quiz(name="testquiz")
+        quiz.save()
+
+    def test_quiz_input_and_output(self):
+        quiz = Quiz.objects.create(name="quiz")
+        getQuiz = Quiz.objects.get(id=quiz.id)
+        self.assertEqual(quiz, getQuiz)
